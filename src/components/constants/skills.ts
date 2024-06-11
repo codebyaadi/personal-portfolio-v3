2664feedb6af631a4d72ce4b9d4e061c183e15f3
codebyaadi <adityastar57@gmail.com>
2024-06-11 22:03:11 +0530

    feat: added the skills section

---
+++ src/components/constants/skills.ts
@@ -0,0 +1,25 @@
+export const skills_data = [
+    {
+        id: 1,
+        title: 'Frontend',
+        techlist: [
+            'HTML',
+            'CSS',
+            'Tailwind',
+            'JavaScript',
+            'TypeScript',
+            'React',
+            'Astro'
+        ]
+    },
+    {
+        id: 2,
+        title: 'Backend',
+        techlist: [
+            'Node',
+            'Bun',
+            'Express',
+            'Hono'
+        ]
+    }
+]
